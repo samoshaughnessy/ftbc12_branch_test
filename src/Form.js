1e10145f8eeb799d1b8187023fcf1603369f0bf3
jteohn <jteohn@gmail.com>
2023-06-23 10:37:30 +0800

    added Form Component

---
+++ src/Form.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault;
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="text"
+            name="text"
+            value={this.setState.name}
+            onChange={this.handleChange}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
