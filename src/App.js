22ab91e497992caa618c9914d626ffa446b8d35d
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-06-23 10:33:02 +0800

    adding new greeting component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Greeting } from "./Greeting";
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <Greeting />
       </header>
     </div>
   );
